6c69747c09bb3e6ecf930604a98562358b8397df
Bruno Lopes de Mello <bruno.guitarpro@gmail.com>
2017-11-18 17:05:48 -0200

    uso do URLSession e JSONSerialization

--- Preco do Bitcoin/ViewController.swift
+++ Preco do Bitcoin/ViewController.swift
@@ -12,7 +12,40 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //define o caminho da API
+        if let url = URL(string: "https://blockchain.info/pt/ticker") {
+            //Define o metodo de requisicao
+            let tarefa = URLSession.shared.dataTask(with: url) { (dados, requisicao, erro) in
+            
+                if erro == nil {
+                    print("Sucesso ao fazer a consulta")
+                    
+                    if let dadosRetorno = dados {
+                        
+                        do{
+                            if let objetoJson = try JSONSerialization.jsonObject(with: dadosRetorno, options: []) as? [String: AnyObject] {
+                                if let brl = objetoJson["BRL"] as? [String: AnyObject]{
+//                                    print(brl)
+                                    if let buy = brl["buy"] as? Double{
+                                        let buyFormatado = String(buy).replacingOccurrences(of: ".", with: ",")
+                                        print(buyFormatado)
+                                    }
+                                }
+                            }
+                        } catch {
+                            print("Erro ao formatar retorno")
+                        }
+                        
+                    }
+                } else {
+                    print("Erro ao fazer a consulta do preco")
+                }
+            }
+            //faz a requisicao
+            tarefa.resume()
+            
+        }
     }
 
     override func didReceiveMemoryWarning() {
