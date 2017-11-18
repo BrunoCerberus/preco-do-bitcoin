add0d8ad97d0e9b8c544e06ec7b1716b901332ed
Bruno Lopes de Mello <bruno.guitarpro@gmail.com>
2017-11-18 19:16:02 -0200

    aplicativo completo

--- Preco do Bitcoin/ViewController.swift
+++ Preco do Bitcoin/ViewController.swift
@@ -7,52 +7,89 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var preco: UILabel!
+    
+    @IBOutlet weak var atualizarButton: UIButton!
+    
+    @IBAction func atualizarPreco(_ sender: Any) {
+        self.recuperarPrecoBitcoin()
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //define o caminho da API
-        if let url = URL(string: "https://blockchain.info/pt/ticker") {
-            //Define o metodo de requisicao
-            let tarefa = URLSession.shared.dataTask(with: url) { (dados, requisicao, erro) in
-            
-                if erro == nil {
-                    print("Sucesso ao fazer a consulta")
-                    
-                    if let dadosRetorno = dados {
-                        
-                        do{
-                            if let objetoJson = try JSONSerialization.jsonObject(with: dadosRetorno, options: []) as? [String: AnyObject] {
-                                if let brl = objetoJson["BRL"] as? [String: AnyObject]{
-//                                    print(brl)
-                                    if let buy = brl["buy"] as? Double{
-                                        let buyFormatado = String(buy).replacingOccurrences(of: ".", with: ",")
-                                        print(buyFormatado)
-                                    }
-                                }
-                            }
-                        } catch {
-                            print("Erro ao formatar retorno")
-                        }
-                        
-                    }
-                } else {
-                    print("Erro ao fazer a consulta do preco")
-                }
-            }
-            //faz a requisicao
-            tarefa.resume()
-            
-        }
+        self.recuperarPrecoBitcoin()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    private func formatarPreco(preco: NSNumber) -> String{
+        let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.locale = Locale(identifier: "pt_BR")
+        
+        if let resultado = nf.string(from: preco) {
+            return resultado
+        }
+        return "0,00"
+    }
 
+    private func recuperarPrecoBitcoin() {
+        
+        self.atualizarButton.setTitle("Atualizando...", for: .normal)
+        
+        
+        Timer.scheduledTimer(withTimeInterval: 3, repeats: false) { (timer) in
+            //define o caminho da API
+            if let url = URL(string: "https://blockchain.info/pt/ticker") {
+                //Define o metodo de requisicao
+                let tarefa = URLSession.shared.dataTask(with: url) { (dados, requisicao, erro) in
+                    
+                    if erro == nil {
+                        print("Sucesso ao fazer a consulta")
+                        
+                        if let dadosRetorno = dados {
+                            
+                            do{
+                                if let objetoJson = try JSONSerialization.jsonObject(with: dadosRetorno, options: []) as? [String: AnyObject] {
+                                    if let brl = objetoJson["BRL"] as? [String: AnyObject]{
+                                        //                                    print(brl)
+                                        if let preco = brl["buy"] as? Double{
+                                            let precoFormatado = self.formatarPreco(preco: NSNumber(value: preco))
+                                            
+                                            //so vai alterar o componente de interface assim que o request for concluido
+                                            DispatchQueue.main.async(execute: {
+                                                self.preco.text = "R$ " + precoFormatado
+                                                self.atualizarButton.setTitle("Atualizar", for: .normal)
+                                                print(precoFormatado)
+                                            })
+                                        }
+                                    }
+                                }
+                            } catch {
+                                print("Erro ao formatar retorno")
+                            }
+                            
+                        }
+                    } else {
+                        print("Erro ao fazer a consulta do preco")
+                    }
+                }
+                //faz a requisicao
+                tarefa.resume()
+                
+            }
+        }
 
+    }
 }
 
